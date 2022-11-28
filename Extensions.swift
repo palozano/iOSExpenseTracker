c6f8660351717ae270107ce1844eae20aabe1d49
Pablo <pablo@t3rn.io>
2022-11-28 23:55:43 +0100

    chore: before pushing to remote

---
+++ Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Pablo Lozano on 28/11/22.
+//
+
+// Extensions.swift
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
